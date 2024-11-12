3e4aac1fda9b7c02e839cd87be86ba0a0cc9d55e
avcnaalksa <its.me.avicena@gmail.com>
2024-11-12 14:41:25 +0700

    Update setup_ubuntu.sh

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -1,1 +1,48 @@
+#!/bin/bash
 
+# Update dan install DHCP server
+sudo apt update && sudo apt install -y isc-dhcp-server iptables-persistent
+
+# Konfigurasi VLAN
+echo "Mengkonfigurasi VLAN..."
+sudo bash -c 'cat > /etc/netplan/01-netcfg.yaml' << EOF
+network:
+  version: 2
+  ethernets:
+    eth0:
+      dhcp4: no
+  vlans:
+    eth0.10:
+      id: 10
+      link: eth0
+      addresses: [192.168.A.1/24]
+      dhcp4: no
+EOF
+
+# Terapkan konfigurasi Netplan
+sudo netplan apply
+
+# Konfigurasi DHCP
+echo "Mengkonfigurasi DHCP..."
+sudo bash -c 'cat > /etc/dhcp/dhcpd.conf' << EOF
+subnet 192.168.A.0 netmask 255.255.255.0 {
+  range 192.168.A.10 192.168.A.100;
+  option routers 192.168.A.1;
+  option domain-name-servers 8.8.8.8, 8.8.4.4;
+}
+EOF
+
+# Restart DHCP server
+sudo systemctl restart isc-dhcp-server
+
+# Aktifkan IP forwarding
+echo "Mengaktifkan IP forwarding..."
+sudo sysctl -w net.ipv4.ip_forward=1
+sudo bash -c 'echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf'
+
+# Konfigurasi NAT agar client dapat mengakses internet
+echo "Mengkonfigurasi NAT..."
+sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+sudo netfilter-persistent save
+
+echo "Konfigurasi Ubuntu Server suksess WELL."
