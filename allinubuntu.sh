0f7e7c44ff044f4e58fab969960729592f0ec8b8
avcnaalksa <its.me.avicena@gmail.com>
2024-11-16 10:38:29 +0700

    Update allinubuntu.sh

--- allinubuntu.sh
+++ allinubuntu.sh
@@ -3,18 +3,21 @@
 # Variabel Konfigurasi
 VLAN_INTERFACE="eth1.10"
 VLAN_ID=10
-IP_ADDR="192.168.24.1/24"      # IP address untuk interface VLAN di Ubuntu
+IP_ADDR="192.168.24.1/24"      # IP address kanggo interface VLAN nang Ubuntu
 DHCP_CONF="/etc/dhcp/dhcpd.conf"
-SWITCH_IP="192.168.24.35"       # IP Cisco Switch yang diperbarui
-MIKROTIK_IP="192.168.200.1"     # IP MikroTik yang baru
-USER_SWITCH="root"              # Username SSH untuk Cisco Switch
+SWITCH_IP="192.168.24.35"       # IP Cisco Switch sing diperbarui
+MIKROTIK_IP="192.168.200.1"     # IP MikroTik sing anyar
+USER_SWITCH="root"              # Username SSH kanggo Cisco Switch
 USER_MIKROTIK="admin"           # Username SSH default MikroTik
-PASSWORD_SWITCH="root"          # Password untuk Cisco Switch
-PASSWORD_MIKROTIK=""            # Kosongkan jika MikroTik tidak memiliki password
+PASSWORD_SWITCH="root"          # Password kanggo Cisco Switch
+PASSWORD_MIKROTIK=""            # Kosongno yen MikroTik ora nduwe password
 
 set -e
 
-# Menambah Repositori Kartolo
+echo "🎉 Skrip Otomasi diwiwiti! Gaspol Rek, saiki jadi Sultan Konfigurasi! 😹"
+
+# Nambah Repositori Kartolo
+echo "🍩 Lagi nambah repo Kartolo... servere ngopi dhisik, ben ora ngambek! ☕"
 cat <<EOF | sudo tee /etc/apt/sources.list
 deb http://kartolo.sby.datautama.net.id/ubuntu/ focal main restricted universe multiverse
 deb http://kartolo.sby.datautama.net.id/ubuntu/ focal-updates main restricted universe multiverse
@@ -27,16 +30,16 @@ sudo apt update
 sudo apt install sshpass -y
 sudo apt install -y isc-dhcp-server iptables iptables-persistent
 
-# 1. Konfigurasi VLAN di Ubuntu Server
-echo "Mengonfigurasi VLAN di Ubuntu Server..."
+# 1. Konfigurasi VLAN nang Ubuntu Server
+echo "🔧 Ngawe VLAN nang Ubuntu... tenang lek, kabel iki tak upgrade dadi kapal Titanic sing ora karam! 😹"
 ip link add link eth1 name $VLAN_INTERFACE type vlan id $VLAN_ID
 ip addr add $IP_ADDR dev $VLAN_INTERFACE
 ip link set up dev $VLAN_INTERFACE
 
 # 2. Konfigurasi DHCP Server
-echo "Menyiapkan konfigurasi DHCP server..."
+echo "📡 Setting DHCP... IP kudu adil rek, ojok rebutan kayak lek dodolan cilok nang pasar! 😎"
 cat <<EOL | sudo tee $DHCP_CONF
-# Konfigurasi subnet untuk VLAN 10
+# Konfigurasi subnet kanggo VLAN 10
 subnet 192.168.24.0 netmask 255.255.255.0 {
     range 192.168.24.10 192.168.24.100;
     option routers 192.168.24.1;
@@ -63,17 +66,17 @@ EOF
 
 sudo netplan apply
 
-# Restart DHCP server untuk menerapkan konfigurasi baru
-echo "Restarting DHCP server..."
+# Restart DHCP server kanggo nerapkan konfigurasi anyar
+echo "🔄 Restart DHCP... tenang, server iki tak pijet sek ben ra spaneng! 😹"
 sudo systemctl restart isc-dhcp-server
 sudo systemctl status isc-dhcp-server
 
-# 3. Konfigurasi Routing di Ubuntu Server
-echo "Menambahkan konfigurasi routing..."
+# 3. Konfigurasi Routing nang Ubuntu Server
+echo "🛣️ Gawe routing... datane saiki lancar rek, kayak dalan tol ora ono lampu abang! 🚦"
 ip route add 192.168.200.0/24 via $MIKROTIK_IP
 
-# 4. Konfigurasi Cisco Switch melalui SSH dengan username dan password root
-echo "Mengonfigurasi Cisco Switch..."
+# 4. Konfigurasi Cisco Switch liwat SSH
+echo "🤖 Ngobrol karo Cisco Switch... ayo ndang rampung, lek switch iki ojok ngambek! 🥲"
 sshpass -p "$PASSWORD_SWITCH" ssh -o StrictHostKeyChecking=no $USER_SWITCH@$SWITCH_IP <<EOF
 enable
 configure terminal
@@ -88,22 +91,22 @@ end
 write memory
 EOF
 
-# 5. Konfigurasi MikroTik melalui SSH tanpa prompt
-echo "Mengonfigurasi MikroTik..."
+# 5. Konfigurasi MikroTik liwat SSH
+echo "📡 Saiki giliran MikroTik... ayo bro, iki tak konfigurasi nganggo skill dewa! 💻"
 if [ -z "$PASSWORD_MIKROTIK" ]; then
     ssh -o StrictHostKeyChecking=no $USER_MIKROTIK@$MIKROTIK_IP <<EOF
 interface vlan add name=vlan10 vlan-id=$VLAN_ID interface=ether1
-ip address add address=192.168.24.1/24 interface=vlan10      # Sesuaikan dengan IP di VLAN Ubuntu
-ip address add address=192.168.200.1/24 interface=ether2     # IP address MikroTik di network lain
+ip address add address=192.168.24.1/24 interface=vlan10      # Sesuaikan karo IP nang VLAN Ubuntu
+ip address add address=192.168.200.1/24 interface=ether2     # IP address MikroTik nang jaringan liyane
 ip route add dst-address=192.168.24.0/24 gateway=192.168.24.1
 EOF
 else
     sshpass -p "$PASSWORD_MIKROTIK" ssh -o StrictHostKeyChecking=no $USER_MIKROTIK@$MIKROTIK_IP <<EOF
 interface vlan add name=vlan10 vlan-id=$VLAN_ID interface=ether1
-ip address add address=192.168.24.1/24 interface=vlan10      # Sesuaikan dengan IP di VLAN Ubuntu
-ip address add address=192.168.200.1/24 interface=ether2     # IP address MikroTik di network lain
+ip address add address=192.168.24.1/24 interface=vlan10      # Sesuaikan karo IP nang VLAN Ubuntu
+ip address add address=192.168.200.1/24 interface=ether2     # IP address MikroTik nang jaringan liyane
 ip route add dst-address=192.168.24.0/24 gateway=192.168.24.1
 EOF
 fi
 
-echo "Otomasi konfigurasi selesai."
+echo "🎉 Skrip rampung! Saiki sampeyan dadi Raja Otomasi, gaspol maneh rek! 👑"
