338aa8edab1afe4ef0c742ec0c58211e9cb8cfbf
avcnaalksa <its.me.avicena@gmail.com>
2024-11-19 10:40:15 +0700

    Update setup_ubuntu.sh

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -1,48 +1,115 @@
 #!/bin/bash
 
-# Update Sama install DHCP server
-sudo apt update && sudo apt install -y isc-dhcp-server iptables-persistent
-
-# Konfigurasi VLAN
-echo "Mengkonfigurasi VLAN..."
-sudo bash -c 'cat > /etc/netplan/01-netcfg.yaml' << EOF
-network:
-  version: 2
-  ethernets:
-    eth0:
-      dhcp4: no
-  vlans:
-    eth0.10:
-      id: 10
-      link: eth0
-      addresses: [192.168.6.1/24]
-      dhcp4: no
-EOF
-
-# Konfigurasi Netplan
-sudo netplan apply
-
-# Konfigurasi DHCP
-echo "Mengkonfigurasi DHCP..."
-sudo bash -c 'cat > /etc/dhcp/dhcpd.conf' << EOF
-subnet 192.168.6.0 netmask 255.255.255.0 {
-  range 192.168.6.10 192.168.6.100;
-  option routers 192.168.6.1;
-  option domain-name-servers 8.8.8.8, 8.8.4.4;
+# Fungsi cetak teks di tengah layar
+print_center() {
+  termwidth=$(tput cols)
+  padding=$(printf '%0.1s' " "{1..500})
+  while IFS= read -r line; do
+    printf '%*.*s%s\n' 0 $(((termwidth - ${#line}) / 2)) "$padding" "$line"
+  done <<< "$1"
 }
+
+# ASCII Art dari input
+ascii_art="
+                                                                                                
+ ____      ____ ____ _____   ______        _____       ______ _____   ______  _________________ 
+|    |    |    |    |\    \ |\     \   ___|\    \  ___|\     |\    \ |\     \/                 \
+|    |    |    |    |\\    \| \     \ /    /\    \|     \     \\    \| \     \______     ______/
+|    |    |    |    | \|    \  \     |    |  |    |     ,_____/\|    \  \     | \( /    /  )/   
+|    |    |    |    |  |     \  |    |    |  |____|     \--'\_|/|     \  |    |  ' |   |   '    
+|    |    |    |    |  |      \ |    |    |   ____|     /___/|  |      \ |    |    |   |        
+|\    \  /    /|    |  |    |\ \|    |    |  |    |     \____|\ |    |\ \|    |   /   //        
+| \ ___\/___ / |____|  |____||\_____/|\ ___\/    /|____ '     /||____||\_____/|  /___//         
+ \ |   ||   | /|    |  |    |/ \|   || |   /____/ |    /_____/ ||    |/ \|   || |`   |          
+  \|___||___|/ |____|  |____|   |___|/\|___|    | |____|     | /|____|   |___|/ |____|          
+    \(    )/     \(      \(       )/    \( |____|/  \( |_____|/   \(       )/     \(            
+     '    '       '       '       '      '   )/      '    )/       '       '       '            
+                                             '            '                                     
+"
+
+# Clear screen and display ASCII art
+clear
+print_center "$ascii_art"
+print_center "           TAMPIL KECE 😹 VINCENT              "
+print_center "+-------------------------------------------+"
+print_center "|            CONFIGURE UBUNTU 20.04         |"
+print_center "|       DHCP + VLAN + NAT + Kartolo         |"
+print_center "+-------------------------------------------+"
+print_center ""
+
+echo "Halo Vincent! Siap setup Ubuntu gaya hacker elite? Let's go! 😹"
+
+# Pastikan skrip dijalankan dengan sudo
+if [[ $EUID -ne 0 ]]; then
+   echo "Jalankan skrip ini sebagai root atau dengan sudo. 😹"
+   exit 1
+fi
+
+# Config IP, VLAN, dan lain-lain
+echo "Mengkonfigurasi IP, VLAN, lan nambah Kartolo Repo kanggo Ubuntu 20.04... 😹"
+
+# Konfigurasi jaringan
+echo "
+auto eth1
+iface eth1 inet static
+address 192.168.6.1
+netmask 255.255.255.0
+" >> /etc/network/interfaces
+
+echo "
+auto eth1.10
+iface eth1.10 inet static
+address 192.168.6.1
+netmask 255.255.255.0
+vlan-raw-device eth1
+" >> /etc/network/interfaces
+
+# Install DHCP server
+echo "Menginstal DHCP server... 😹"
+apt update
+apt install -y isc-dhcp-server
+
+cat <<EOT > /etc/dhcp/dhcpd.conf
+subnet 192.168.6.0 netmask 255.255.255.0 {
+    range 192.168.6.50 192.168.6.100;
+    option routers 192.168.6.1;
+    option domain-name-servers 8.8.8.8, 8.8.4.4;
+
+    # Konfigurasi fix IP untuk VPC Fantasia
+    host fantasia {
+        hardware ethernet 00:11:22:33:44:55; # Ganti dengan MAC address
+        fixed-address 192.168.6.10;
+    }
+}
+EOT
+
+# DHCP server untuk eth1
+sed -i 's/INTERFACESv4=""/INTERFACESv4="eth1"/' /etc/default/isc-dhcp-server
+
+# Forwarding dan NAT
+echo 1 > /proc/sys/net/ipv4/ip_forward
+sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/' /etc/sysctl.conf
+iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+iptables-save > /etc/iptables/rules.v4
+
+# Tambahkan Kartolo repo
+echo "Nambah Kartolo Repo... 😹"
+cat <<EOF >> /etc/apt/sources.list
+# Kartolo Repo untuk Ubuntu 20.04
+deb http://kartolo.sby.datautama.net.id/ubuntu/ focal main restricted universe multiverse
+deb http://kartolo.sby.datautama.net.id/ubuntu/ focal-updates main restricted universe multiverse
+deb http://kartolo.sby.datautama.net.id/ubuntu/ focal-security main restricted universe multiverse
+deb http://kartolo.sby.datautama.net.id/ubuntu/ focal-backports main restricted universe multiverse
+deb http://kartolo.sby.datautama.net.id/ubuntu/ focal-proposed main restricted universe multiverse
 EOF
 
-# Restart DHCP Server
-sudo systemctl restart isc-dhcp-server
+# Update sistem
+echo "Update sistem nganggo repo Kartolo... 😹"
+apt update
 
-# Aktifin IP forwarding
-echo "Mengaktifkan IP forwarding..."
-sudo sysctl -w net.ipv4.ip_forward=1
-sudo bash -c 'echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf'
+# Restart layanan
+echo "Restarting networking dan DHCP services... 😹"
+systemctl restart networking
+systemctl restart isc-dhcp-server
 
-# Konfigurasi NAT biar client dapat akses internet
-echo "Mengkonfigurasi NAT..."
-sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
-sudo netfilter-persistent save
-
-echo "Konfigurasi Ubuntu Server suksess WELL."
+echo "Konfigurasi rampung, Vincent! Sekarang jaringannya udah siap tempur! 😹"
